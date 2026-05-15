ff71b4b04520079fe3fbf6a6d5ea6c49e8457d2f
Anamul Hasan <anamul.jobhunt@gmail.com>
2026-05-14 22:03:26 -0500

    Organize product routes and implement GET operations for all products and single product

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,23 +1,65 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (
-    req : IncomingMessage,
-    res : ServerResponse
+  req: IncomingMessage,
+  res: ServerResponse
 ) => {
-    const url = req.url
-    const method = req.method
+  const url = req.url;
+  const method = req.method;
 
-    if(url === "/products" && method === "GET") {
-         const products = [
-            {
-                id : 1,
-                name: "Product - 1",
-            }
-        ];
+  const urlParts = url?.split("/");
+  const productId = urlParts?.[2];
+  const id = productId ? Number(productId) : null;
 
-        res.writeHead(200, { "content-type" : "application/json"});
-        res.end(
-            JSON.stringify({message: "Products retrieved successfully", data: products})
-        );
+  // Get All Products
+  if (url === "/products" && method === "GET") {
+    const products = readProduct();
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Products retrieved successfully",
+        data: products,
+      })
+    );
+
+    return;
+  }
+
+  // Get Single Product
+  if (method === "GET" && id !== null && !Number.isNaN(id)) {
+    const products = readProduct();
+    const product = products.find((p: IProduct) => p.id === id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Product not found",
+        })
+      );
+
+      return;
     }
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product retrieved successfully",
+        data: product,
+      })
+    );
+
+    return;
+  }
+
+  // Fallback response
+  res.writeHead(404, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      message: "Product route not found",
+    })
+  );
 };
